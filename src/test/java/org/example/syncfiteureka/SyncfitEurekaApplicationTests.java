899a6ade68f790aaee970169f59f0e593b19dc9f
willjsw <sunwoo1137@gmail.com>
2025-03-28 12:53:14 +0900

    init: eureka server init

---
+++ src/test/java/org/example/syncfiteureka/SyncfitEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.syncfiteureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncfitEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
