bb7e30ae90deae6568e2abd44641ce642463a328
willjsw <sunwoo1137@gmail.com>
2025-03-30 19:35:18 +0900

    refactor: 서비스 이름 변경, config-server 등록

--- src/main/java/org/example/syncfiteureka/ServiceDiscoveryApplication.java
+++ src/main/java/org/example/syncfiteureka/ServiceDiscoveryApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class SyncfitEurekaApplication {
+public class ServiceDiscoveryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SyncfitEurekaApplication.class, args);
+        SpringApplication.run(ServiceDiscoveryApplication.class, args);
     }
 
 }
